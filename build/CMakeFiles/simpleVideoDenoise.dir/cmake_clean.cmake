7e06b1ce1b71295df7b1ec1910592676ab1875a6
hetaomilk123 <hetaomilk123@126.com>
2017-10-31 04:40:53 -0700

    Add directory build

---
+++ build/CMakeFiles/simpleVideoDenoise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simpleVideoDenoise.dir/src/main.cpp.o"
+  "../bin/simpleVideoDenoise.pdb"
+  "../bin/simpleVideoDenoise"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/simpleVideoDenoise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
